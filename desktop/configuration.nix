f5bee954fbf95c6d0be9c3c1536ee98374fdb5ae
tttt369 <tadokoro123@outlook.com>
2025-04-28 04:55:46 +0000

    Update flake.nix and add home.nix

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -84,22 +84,23 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
    environment.systemPackages = with pkgs; [
-     neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     wget
-     git
-     gh
-     floorp
-     libsForQt5.fcitx5-configtool
-     btop
-     nautilus
-     copyq
-     fish
-     ranger
-     xclip
-     python3
-     curl
-     unzip
-     ripgrep
+    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+    gh
+    floorp
+    libsForQt5.fcitx5-configtool
+    btop
+    nautilus
+    copyq
+    fish
+    ranger
+    xclip
+    python3
+    curl
+    unzip
+    ripgrep
+    home-manager
    ];
 
   # Some programs need SUID wrappers, can be configured further or are
--- desktop/flake.nix
+++ desktop/flake.nix
@@ -2,17 +2,27 @@
   description = "A very basic flake";
 
   inputs = {
-      # unstableチャンネル
       nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-      # 24.11安定チャンネル
       nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
-    };
+      home-manager = {
+	url = "github:nix-community/home-manager";
+	inputs.nixpkgs.follows = "nixpkgs-unstable";
+      };
+  };
 
-    outputs = { self, nixpkgs-unstable, nixpkgs-stable}: {
-      nixosConfigurations.asdf = nixpkgs-unstable.lib.nixosSystem {
+  outputs = { self, nixpkgs-unstable, nixpkgs-stable, home-manager }: {
+    nixosConfigurations = {
+      asdf = nixpkgs-unstable.lib.nixosSystem {
 	system = "x86_64-linux";
 	modules = [
 	  ./configuration.nix
+	  home-manager.nixosModules.home-manager {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.asdf = {
+	      imports = [ ./home.nix ];
+	    };
+	  }
 	];
 	specialArgs = {
 	  # 24.11のpkgsをconfiguration.nixで利用可能にする
@@ -22,4 +32,5 @@
 	};
       };
     };
+  };
 }
