4b806e6c4bfb20b90c2331878bee1a3fd10ca98f
tttt369 <tadokoro123@outlook.com>
2025-05-11 11:43:41 +0900

    add some change

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -213,20 +213,5 @@
     
   };
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
-
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-      "steam"
-      "steam-original"
-      "steam-unwrapped"
-      "steam-run"
-    ];
-
-
   networking.firewall.checkReversePath = false;
 }
